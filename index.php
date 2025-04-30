e34d929b9284e541be98e94ec651f21116dfb992
Lynn Lukose <lynn.lukose@stu.ocsb.ca>
2025-04-30 14:24:19 +0000

    edit index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
     </header>
     <main class="mdl-layout__content">
       <div class="right-image">
-        <img src="./images/area_of_square.jpg" alt="laptop image" width="250" />
+        <img src="./images/income_tax.jpg" alt="taxes image" width="250" />
       </div>
       <br />
       <div class="page-content">Please enter your employment information.</div>
